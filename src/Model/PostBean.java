5b42e8a7dbda47977306b832c1ba8d75828739a2
stefanolinguiti <s.linguiti@studenti.unisa.it>
2019-01-28 11:53:01 +0100

    roba

--- src/Model/PostBean.java
+++ src/Model/PostBean.java
@@ -1,6 +1,6 @@
 package Model;
 
-import java.util.Date;
+import java.sql.Date;
 
 public class PostBean {
 	private String idPost;
--- src/Model/UtenteBean.java
+++ src/Model/UtenteBean.java
@@ -1,6 +1,6 @@
 package Model;
 
-import java.util.Date;
+import java.sql.Date;
 
 public class UtenteBean 
 {
